3d0572d85f3b2e0ea737075784371c1af547a246
pariraj18 <pariraj18092006@gmal.com>
2026-06-01 09:24:20 +0530

    Wekk 1 Day 1 3 programs

--- Week 1/Day 1/1_TwoSum.java
+++ Week 1/Day 1/1_TwoSum.java
@@ -1,4 +1,7 @@
+import java.util.*;
 class Solution {
+
+
     public int[] twoSum(int[] nums, int target) {
         Map<Integer,Integer>map=new HashMap<>();
         for(int i=0;i<nums.length;i++){
