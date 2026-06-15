fc18e3b38e51a7fd7c7271a767f47f33e7921bcb
pariraj18 <pariraj18092006@gmal.com>
2026-06-15 07:09:42 +0530

    Week 3 Day 1 3 programs solved

---
+++ Week3/Day1/1_LinkedListCycle.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode f=head;
+        ListNode s=head;
+        while(f!=null && f.next!=null){
+            s=s.next;
+            f=f.next.next;
+            if(s==f){return true;}
+        }
+      return false;  
+    }
+}
---
+++ Week3/Day1/2_ReverseLinkedList.java
@@ -0,0 +1,15 @@
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev=null;
+        ListNode temp=head;
+        while(temp!=null){
+            ListNode next=temp.next;
+            temp.next=prev;
+            prev=temp;
+            temp=next;
+
+        }
+        return prev;
+        
+    }
+
