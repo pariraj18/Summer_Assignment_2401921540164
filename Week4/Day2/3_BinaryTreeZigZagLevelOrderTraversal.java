b0b5281e7c7ebb27b9e1293cb84610b8f6ada4f3
pariraj18 <pariraj18092006@gmal.com>
2026-06-26 08:00:42 +0530

    Renamed WEEK4 to Week4 and added files

---
+++ Week4/Day2/3_BinaryTreeZigZagLevelOrderTraversal.java
@@ -0,0 +1,40 @@
+ import java.util.List;
+ import java.util.ArrayList;
+ class Solution {
+    public static int height(TreeNode root){
+        if(root==null || (root.left==null && root.right==null))return 0;
+        return 1+Math.max(height(root.left),height(root.right));
+    }
+    public void nthLevel(TreeNode root,int n,List<Integer>arr){
+        //left to right
+        if(root==null)return;
+        if(n==1){
+            arr.add(root.val);
+            return;
+        }
+        nthLevel(root.left,n-1,arr);
+        nthLevel(root.right,n-1,arr);
+    }
+    public void nthLevel2(TreeNode root,int n,List<Integer>arr){
+        //right to left
+        if(root==null)return;
+        if(n==1){
+            arr.add(root.val);
+            return;
+        }
+        nthLevel2(root.right,n-1,arr);
+        nthLevel2(root.left,n-1,arr);
+    }
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+      int level=height(root)+1;
+      List<List<Integer>>ans=new ArrayList<>();
+      if(root==null)return ans;
+      for(int i=1;i<=level;i++){
+        List<Integer>arr=new ArrayList<>();
+        if(i%2!=0)nthLevel(root,i,arr);
+        else nthLevel2(root,i,arr);
+        ans.add(arr);
+      }
+      return ans;
+    }
+}
---
+++ Week4/Day4/2_DiameterOfBinaryTree.jav
@@ -0,0 +1,18 @@
+class Solution {
+    public static int height (TreeNode root){
+        if(root==null || (root.left==null && root.right==null))return 0;
+        return 1+Math.max(height(root.left),height(root.right));
+
+    }
+    public int diameterOfBinaryTree(TreeNode root) {
+        if(root==null || (root.left==null && root.right==null))return 0;
+        int lsd=diameterOfBinaryTree(root.left);
+        int rsd=diameterOfBinaryTree(root.right);
+        int mid=height(root.left)+height(root.right);
+        if(root.left!=null)mid++;
+        if(root.right!=null)mid++;
+        int maxDIA=Math.max(lsd,Math.max(rsd,mid));
+        return maxDIA;
+        
+    }
+}
